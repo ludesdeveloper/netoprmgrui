3e2dfbe9786d6003d91d3f9e64cb389b4ef7cd18
Seski Ramadhan <seski.ramadhan@gmail.com>
2020-08-22 10:41:53 +0700

    update option to compare ui

--- lib/help_tab.dart
+++ lib/help_tab.dart
@@ -3,49 +3,49 @@ import 'package:flutter/material.dart';
 class HelpTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: DefaultTabController(
-        length: 4,
-        child: Scaffold(
-          appBar: AppBar(
-            flexibleSpace: Center(
-              child: SafeArea(
-                child: TabBar(isScrollable: true, tabs: <Widget>[
-                  Tab(
-                    child: Text('How to Use'),
-                  ),
-                  Tab(
-                    child: Text('Command Reference Guide'),
-                  ),
-                  Tab(
-                    child: Text('Support Device'),
-                  ),
-                  Tab(
-                    child: Text('Developer'),
-                  ),
-                ]),
+    return Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              RaisedButton(
+                onPressed: () {},
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(18.0),
+                ),
+                child: Text('How to Use'),
               ),
-            ),
-            // bottom:
-          ),
-          body: TabBarView(
-            children: <Widget>[
-              Container(
-                child: Center(child: Text('Put Class Here')),
-              ),
-              Container(
-                child: Center(child: Text('Put Class Here')),
-              ),
-              Container(
-                child: Center(child: Text('Put Class Here')),
-              ),
-              Container(
-                child: Center(child: Text('Put Class Here')),
+              RaisedButton(
+                onPressed: () {},
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(18.0),
+                ),
+                child: Text('Command Reference Guide'),
               ),
             ],
           ),
-        ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              RaisedButton(
+                onPressed: () {},
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(18.0),
+                ),
+                child: Text('Support Device'),
+              ),
+              RaisedButton(
+                onPressed: () {},
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(18.0),
+                ),
+                child: Text('Developer'),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
